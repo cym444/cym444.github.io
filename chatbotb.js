edf8cb19029c27176e5c03fad054c49f33703e98
maggie_li <pcm755@gmail.com>
2026-05-07 10:45:07 +0800

    Update chatbotb.js

--- chatbotb.js
+++ chatbotb.js
@@ -3,7 +3,7 @@ class ChatBot {
     constructor() {
         // ⚠️ Important: Replace with your n8n webhook URL
         // ****************************************************************
-        this.webhookUrl = 'https:/lutalica444.app.n8n.cloud/webhook/chatb';
+        this.webhookUrl = 'https://lutalica444.app.n8n.cloud/webhook-test/chatb';
         // ****************************************************************        
         // Initialize session ID
         this.sessionId = this.getOrCreateSessionId();
